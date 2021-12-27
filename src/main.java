d999a10834280fe52de66f9aacee7a74586c8beb
karasito <karasito@mail.ru>
2021-12-27 21:49:31 +0300

    Initial commit

---
+++ src/main.java
@@ -0,0 +1,9 @@
+public class main {
+    public static void main(String[] args) {
+
+        int airTicket = 200 ;
+        int bonus = 20 ;
+        int bonusMyles = airTicket/bonus ;
+        System.out.println("Ваши бонусные мили: " + bonusMyles);
+    }
+}
