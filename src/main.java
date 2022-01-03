b93130feee07f6784b99f524d241d433a7e99456
karasito <94555947+karasito@users.noreply.github.com>
2022-01-03 18:16:39 +0300

    Update main.java

--- src/main.java
+++ src/main.java
@@ -1,9 +1,9 @@
-public class main {
+public class Main {
     public static void main(String[] args) {
 
-        int airTicket = 200 ;
-        int bonus = 20 ;
-        int bonusMyles = airTicket/bonus ;
+        int airTicket = 200;
+        int bonus = 20;
+        int bonusMyles = airTicket / bonus;
         System.out.println("Ваши бонусные мили: " + bonusMyles);
     }
 }
